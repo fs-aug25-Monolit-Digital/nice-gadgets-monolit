847fccaa09b2d1afa119737168a841c851519cfd
Danylo <lliivadin@gmail.com>
2025-11-19 16:48:07 +0200

    feat(MD-20): create base structure for favorites page

--- src/components/pages/FavoritesPage.tsx
+++ src/components/pages/FavoritesPage.tsx
@@ -1,9 +1,12 @@
 import type React from "react";
 import { Footer } from "../organisms/Footer";
+import { Header } from "../organisms/Header";
 
 export const FavoritesPage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
+      <Header />
+
       <main className="flex-grow container mx-auto px-4 md:px-6 xl:px-0 max-w-[1136px]">
         <div className="mb-8">
           <h1 className="text-[32px] md:text-[48px] font-bold text-primary mb-2">Favourites</h1>
@@ -11,7 +14,7 @@ export const FavoritesPage: React.FC = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-10 mb-20">
-          <p>Phone</p>
+          
         </div>
       </main>
 
