c0c507b3da2dac6d5af7567ead3b0011e7defcf5
Danylo <lliivadin@gmail.com>
2025-11-21 00:14:38 +0200

    fix(MD-25): fix types CategoryProduct

--- src/types/CategoryProduct.tsx
+++ src/types/CategoryProduct.tsx
@@ -12,27 +12,20 @@ export type CategoryProduct = {
   category: Category;
   namespaceId: string;
   name: string;
-
   capacityAvailable: string[];
   capacity: string;
-
   priceRegular: number;
   priceDiscount?: number;
-
   colorsAvailable: string[];
   color: string;
-
   images: string[];
   description: DescriptionItem[];
-
   screen: string;
   resolution: string;
   processor: string;
   ram: string;
-
   camera?: string;
   zoom?: string;
-
   cell: string[];
 };
 
